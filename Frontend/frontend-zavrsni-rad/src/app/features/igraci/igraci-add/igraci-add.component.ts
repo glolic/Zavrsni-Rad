aa2841f1575c7b87c814c09e9e5bce39043b1528
glolic <glolic@tvz.hr>
2020-06-21 16:13:25 +0200

    e sad je final igraci

--- Frontend/frontend-zavrsni-rad/src/app/features/igraci/igraci-add/igraci-add.component.ts
+++ Frontend/frontend-zavrsni-rad/src/app/features/igraci/igraci-add/igraci-add.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Igrac } from 'src/app/modeli/igrac-model';
+import { FormControl } from '@angular/forms';
+import { Osoba } from 'src/app/modeli/osoba-model';
+import { Pozicija } from 'src/app/modeli/pozicija-model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OsobeService } from '../../services/osobe-service';
+import { PozicijeService } from '../../services/pozicije-service';
+import { IgraciService } from '../../services/igraci-service';
 
 @Component({
   selector: 'app-igraci-add',
@@ -7,9 +15,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IgraciAddComponent implements OnInit {
 
-  constructor() { }
+  person = new FormControl('');
+  jerseyNumber = new FormControl('');
+  position = new FormControl('');
+
+  personCollection: Osoba[];
+  positionCollection: Pozicija[];
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private osobaService: OsobeService,
+    private pozicijaService: PozicijeService,
+    private igracService: IgraciService) { }
 
   ngOnInit() {
+    
+  }
+
+  ngAfterContentInit() {
+    this.osobaService.getAllPersons().subscribe(
+      (data) => {
+        this.personCollection = data;
+      }
+    );
+
+    this.pozicijaService.getAllPositions().subscribe(
+      (data) => {
+        this.positionCollection = data;
+      }
+    );
+  }
+
+  gotoList() {
+    this.router.navigate(['/igraci']);
+  }
+
+  onSubmit(){
+    if (this.person.valid) {
+      let igrac = new Igrac(null, this.person.value, this.position.value, this.jerseyNumber.value);
+      this.igracService.add(igrac).subscribe(
+        response => {this.gotoList()}
+      );
+    }
+    else {
+      this.person.markAsTouched();
+    }
+  }
+
+  displayFn1(osoba: Osoba): string {
+    return osoba && osoba.ime + ' ' + osoba.prezime ? osoba.ime + ' ' + osoba.prezime : '';
+  }
+
+  displayFn2(pozicija: Pozicija): string {
+    return pozicija && pozicija.naziv ? pozicija.naziv : '';
   }
 
 }
